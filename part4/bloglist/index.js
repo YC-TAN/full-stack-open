d8cc793d659e8d0c5aa170b4f4945463ba5189f1
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-01-08 14:17:26 +1300

    Refractor

---
+++ part4/bloglist/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI, { family: 4 })
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
