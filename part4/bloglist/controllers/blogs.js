7557b4dcf153de074ea1fc337e5f8735f47da9c7
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-01-15 12:01:26 +1300

    Update blog model and route to include creator

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,23 +1,84 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const userExtractor = require('../utils/middleware').userExtractor
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({})
+    .populate('user', {username: 1, name: 1})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog
+    .findById(request.params.id)
+    .populate('user', {username: 1, name: 1})
+
+  if (blog) {
+    return response.json(blog)
+  } else {
+    return response.status(404).end()
+  }
+})
+
+blogsRouter.post('/', userExtractor, async (request, response) => {
+  const body = request.body 
+
+  const user = request.user
+
   const blog = new Blog({
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes ?? 0,
+    user: user._id
   })
 
-  blog.save().then((savedBlog) => {
-    response.status(201).json(savedBlog)
-  })
+  const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+
+  const user = request.user
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) return response.status(404).json({error: "blog not found"})
+
+  if (blog.user.toString() === user._id.toString()) {
+    await Blog.findByIdAndDelete(request.params.id)
+
+    response.status(204).end()
+  } else {
+    response.status(401).json({error: 'only the creator can delete this blog'})
+  }
+})
+
+blogsRouter.put('/:id', userExtractor, async (request, response) => {
+  const user = request.user
+  
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(404).json({error: "blog not found"}) 
+  } 
+
+  if (blog.user.toString() !== user._id.toString()) {
+    return response.status(401).json({error: 'only the creator can delete this blog'})
+  }
+  
+  blog.likes = request.body.likes
+  const updatedBlog = await blog.save()
+  
+
+  response.status(200).json(updatedBlog)  
 })
 
 module.exports = blogsRouter
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-  title: String,
+  title: {
+    type: String,
+    required: true
+  },
   author: String,
-  url: String,
-  likes: Number,
+  url: {
+    type: String,
+    required: true
+  },
+  likes: {
+    default: 0,
+    type: Number, 
+    min: 0
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
