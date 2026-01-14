33184707e8d4d45a0db8fb9f21a6a30d048aa55f
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-01-15 11:57:52 +1300

    Add users routes

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,31 @@
+const User = require('../models/user')
+const usersRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+        .populate('blogs', {url: 1, title: 1, author: 1})
+    response.status(200).json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body
+
+    if (username.length < 3 || password.length < 3) {
+        return response.status(400).json({error: 'password and username must be at least 3 characters long'})
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
